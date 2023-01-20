acb039076fc91042976f3e2ab42071593d909f78
clemPera <clement.pera@gmail.com>
2023-01-20 14:50:13 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,410 @@
+import network
+import socket
+import struct
+import time
+
+from machine import Pin, SPI
+import framebuf
+import utime
+
+import urequests
+import json
+import random
+
+WF_PARTIAL_2IN13_V3= [
+    0x0,0x40,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x80,0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x40,0x40,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x14,0x0,0x0,0x0,0x0,0x0,0x0,  
+    0x1,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x1,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x22,0x22,0x22,0x22,0x22,0x22,0x0,0x0,0x0,
+    0x22,0x17,0x41,0x00,0x32,0x36,
+]
+
+WS_20_30_2IN13_V3 = [ 
+    0x80,0x4A,0x40,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x40,0x4A,0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x80,0x4A,0x40,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x40,0x4A,0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
+    0xF,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0xF,0x0,0x0,0xF,0x0,0x0,0x2,    
+    0xF,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0x1,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0x0,0x0,0x0,0x0,0x0,0x0,0x0,    
+    0x22,0x22,0x22,0x22,0x22,0x22,0x0,0x0,0x0,  
+    0x22,0x17,0x41,0x0,0x32,0x36  
+]
+
+EPD_WIDTH       = 122
+EPD_HEIGHT      = 250
+
+RST_PIN         = 12
+DC_PIN          = 8
+CS_PIN          = 9
+BUSY_PIN        = 13
+
+class EPD_2in13_V3_Landscape(framebuf.FrameBuffer):
+    def __init__(self):
+        self.reset_pin = Pin(RST_PIN, Pin.OUT)
+        
+        self.busy_pin = Pin(BUSY_PIN, Pin.IN, Pin.PULL_UP)
+        self.cs_pin = Pin(CS_PIN, Pin.OUT)
+        if EPD_WIDTH % 8 == 0:
+            self.width = EPD_WIDTH
+        else :
+            self.width = (EPD_WIDTH // 8) * 8 + 8
+
+        self.height = EPD_HEIGHT
+        
+        self.full_lut = WF_PARTIAL_2IN13_V3
+        self.partial_lut = WS_20_30_2IN13_V3
+        
+        self.spi = SPI(1)
+        self.spi.init(baudrate=4000_000)
+        self.dc_pin = Pin(DC_PIN, Pin.OUT)
+
+        self.buffer = bytearray(self.height * self.width // 8)
+        super().__init__(self.buffer, self.height, self.width, framebuf.MONO_VLSB)
+        self.init()
+
+    def digital_write(self, pin, value):
+        pin.value(value)
+
+    def digital_read(self, pin):
+        return pin.value()
+
+    def delay_ms(self, delaytime):
+        utime.sleep(delaytime / 1000.0)
+
+    def spi_writebyte(self, data):
+        self.spi.write(bytearray(data))
+
+    def reset(self):
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(20)
+        self.digital_write(self.reset_pin, 0)
+        self.delay_ms(2)
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(20)   
+
+    def send_command(self, command):
+        self.digital_write(self.dc_pin, 0)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([command])
+        self.digital_write(self.cs_pin, 1)
+
+    def send_data(self, data):
+        self.digital_write(self.dc_pin, 1)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([data])
+        self.digital_write(self.cs_pin, 1)
+
+    def ReadBusy(self):
+        #print('busy')
+        self.delay_ms(10)
+        while(self.digital_read(self.busy_pin) == 1):      # 0: idle, 1: busy
+            self.delay_ms(10)    
+        #print('busy release')
+
+    def TurnOnDisplay(self):
+        self.send_command(0x22)  # Display Update Control
+        self.send_data(0xC7)
+        self.send_command(0x20)  #  Activate Display Update Sequence    
+        self.ReadBusy()
+
+    def TurnOnDisplayPart(self):
+        self.send_command(0x22)  # Display Update Control
+        self.send_data(0x0F)     # fast:0x0c, quality:0x0f, 0xcf
+        self.send_command(0x20)  # Activate Display Update Sequence 
+        self.ReadBusy()
+
+    def LUT(self, lut):
+        self.send_command(0x32)
+        for i in range(0,153):
+            self.send_data(lut[i])
+        self.ReadBusy()
+
+    def LUT_by_host(self, lut):
+        self.LUT(lut)             # lut
+        self.send_command(0x3F)
+        self.send_data(lut[153])
+        self.send_command(0x03)   # gate voltage
+        self.send_data(lut[154])
+        self.send_command(0x04)   # source voltage
+        self.send_data(lut[155])  # VSH
+        self.send_data(lut[156])  # VSH2
+        self.send_data(lut[157])  # VSL
+        self.send_command(0x2C)   # VCOM
+        self.send_data(lut[158])
+
+    def SetWindows(self, Xstart, Ystart, Xend, Yend):
+        self.send_command(0x44)                #  SET_RAM_X_ADDRESS_START_END_POSITION
+        self.send_data((Xstart >> 3) & 0xFF)
+        self.send_data((Xend >> 3) & 0xFF)
+        
+        self.send_command(0x45)                #  SET_RAM_Y_ADDRESS_START_END_POSITION
+        self.send_data(Ystart & 0xFF)
+        self.send_data((Ystart >> 8) & 0xFF)
+        self.send_data(Yend & 0xFF)
+        self.send_data((Yend >> 8) & 0xFF)
+
+    def SetCursor(self, Xstart, Ystart):
+        self.send_command(0x4E)             #  SET_RAM_X_ADDRESS_COUNTER
+        self.send_data(Xstart & 0xFF)
+        
+        self.send_command(0x4F)             #  SET_RAM_Y_ADDRESS_COUNTER
+        self.send_data(Ystart & 0xFF)
+        self.send_data((Ystart >> 8) & 0xFF)
+
+    def init(self):
+        print('init')
+        self.reset()
+        self.delay_ms(100)
+        
+        self.ReadBusy()
+        self.send_command(0x12)  # SWRESET
+        self.ReadBusy()
+        
+        self.send_command(0x01)  # Driver output control 
+        self.send_data(0xf9)
+        self.send_data(0x00)
+        self.send_data(0x00)
+        
+        self.send_command(0x11)  #data entry mode 
+        self.send_data(0x07)
+        
+        self.SetWindows(0, 0, self.width-1, self.height-1)
+        self.SetCursor(0, 0)
+        
+        self.send_command(0x3C)  # BorderWavefrom
+        self.send_data(0x05)
+        
+        self.send_command(0x21) # Display update control
+        self.send_data(0x00)
+        self.send_data(0x80)
+        
+        self.send_command(0x18) # Read built-in temperature sensor
+        self.send_data(0x80)
+        
+        self.ReadBusy()
+        self.LUT_by_host(self.partial_lut)
+
+    def Clear(self):
+        self.send_command(0x24)
+        for j in range(int(self.width / 8) - 1, -1, -1):
+            for i in range(0, self.height):
+                self.send_data(0xFF)
+                
+        self.TurnOnDisplay()    
+
+    def display(self, image):
+        self.send_command(0x24)
+        for j in range(int(self.width / 8) - 1, -1, -1):
+            for i in range(0, self.height):
+                self.send_data(image[i + j * self.height])
+
+        self.TurnOnDisplay()
+
+    def Display_Base(self, image):
+        self.send_command(0x24)
+        for j in range(int(self.width / 8) - 1, -1, -1):
+            for i in range(0, self.height):
+                self.send_data(image[i + j * self.height])
+                
+        self.send_command(0x26)
+        for j in range(int(self.width / 8) - 1, -1, -1):
+            for i in range(0, self.height):
+                self.send_data(image[i + j * self.height])
+                
+        self.TurnOnDisplay()
+        
+    def display_Partial(self, image):
+        self.digital_write(self.reset_pin, 0)
+        self.delay_ms(1)
+        self.digital_write(self.reset_pin, 1)
+        
+        self.LUT_by_host(self.full_lut)
+        
+        self.send_command(0x37)
+        self.send_data(0x00)
+        self.send_data(0x00)
+        self.send_data(0x00)
+        self.send_data(0x00)
+        self.send_data(0x00)
+        self.send_data(0x40)
+        self.send_data(0x00)
+        self.send_data(0x00)
+        self.send_data(0x00)
+        self.send_data(0x00)
+        self.send_data(0x00)
+        
+        self.send_command(0x3C)
+        self.send_data(0x80)
+        
+        self.send_command(0x22)
+        self.send_data(0xC0)
+        self.send_command(0x20)
+        self.ReadBusy()
+        
+        self.SetWindows(0,0,self.width-1,self.height-1)
+        self.SetCursor(0,0)
+        
+        self.send_command(0x24)
+        for j in range(int(self.width / 8) - 1, -1, -1):
+            for i in range(0, self.height):
+                self.send_data(image[i + j * self.height])
+                
+        self.TurnOnDisplayPart()
+    
+    def sleep(self):
+        self.send_command(0x10) #enter deep sleep
+        self.send_data(0x01)
+        self.delay_ms(100)
+
+def connect_wifi():
+    ssid = 'YourWiFi' #Replace with your ssid
+    password = 'YourWifiPassword' #Replace with your wifi password
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+    max_wait = 10
+    while max_wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        max_wait -= 1
+        print('waiting for connection...')
+        time.sleep(1)
+
+    if wlan.status() != 3:
+        raise RuntimeError('network connection failed')
+    else:
+        print('connected')
+        status = wlan.ifconfig()
+        print( 'ip = ' + status[0] )
+        
+        
+def set_time(): #Set the time
+    NTP_DELTA = 2208988800 + 18000 #Modify the value after + (or replace with -) for your timezone : +3600 = UTC+1  / +18000 = 5*3600 = UTC+5  
+    host = "pool.ntp.org"
+    NTP_QUERY = bytearray(48)
+    NTP_QUERY[0] = 0x1B
+    addr = socket.getaddrinfo(host, 123)[0][-1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.settimeout(1)
+        res = s.sendto(NTP_QUERY, addr)
+        msg = s.recv(48)
+    finally:
+        s.close()
+    val = struct.unpack("!I", msg[40:44])[0]
+    t = val - NTP_DELTA    
+    tm = time.gmtime(t)
+    machine.RTC().datetime((tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0))
+
+def request():
+    allQuote = urequests.get("https://www.reddit.com/r/quotes/top/.json?t=week&limit=10")
+    j = json.loads(allQuote.text)
+    
+    numQuo = len(j['data']['children']) #number of quote
+    
+    randQuo = random.randrange(0,numQuo) #choose a random quote
+    
+    text = j['data']['children'][randQuo]['data']['title']
+    return(text) #Return only the title of the reddit post
+
+if __name__=='__main__':
+     connect_wifi() #Don't forget to replace value in this function
+     set_time()
+     
+     #Setup and initalise e-ink screen
+     epd = EPD_2in13_V3_Landscape()
+     epd.Clear()
+     epd.init()
+     epd.fill(0xff)
+     
+     fullClear = True
+     oldDate = 0
+     line = 0
+     
+     while True:
+         now = list(time.localtime())
+         
+         #If it is a digit, add 0 in front to make it easier to read.
+         for x in range(1,5):
+             if now[x] < 10:
+                 now[x] = "0" + str(now[x])
+         
+         date = "{}/{}/{}".format(now[2], now[1], now[0])
+         hourMin = "{}:{}".format(now[3], now[4])
+         
+         if date != oldDate or line > 80: #If the date has changed or the quote is too long, update the quote.
+             epd.fill(0xff)
+             
+             quote = request()
+             print(quote)
+
+             #Split the quote
+             quoteSplit = quote.split()
+
+             total=0
+             line = 10
+
+             while total < len(quoteSplit): #Place text on screen
+                 listTmp = []
+                 a=0
+                
+                 while len(' '.join(listTmp)) < 24 and total < len(quoteSplit): #Place lines of the quote on screen
+                     listTmp.insert(a, quoteSplit[total])
+                     a = a + 1
+                     total = total + 1
+                 
+                 line = line + 10
+                 epd.text((' '.join(listTmp)).center(31), 0, line, 0x00) #print quote on edp
+             
+             oldDate = date
+             fullClear = True
+         
+         #Update text on screen
+         if(fullClear == True):
+             print("Updating quote and date/time...")
+             epd.fill_rect(0, 106, 50, 50, 0xff)
+             epd.text(date, 0, 106, 0x00)
+             
+             epd.fill_rect(200, 106, 50, 50, 0xff)
+             epd.text(hourMin, 200, 106, 0x00)
+             
+             epd.display(epd.buffer)
+             fullClear = False
+         elif oldTime != hourMin:
+             print("Updating date/time...")
+             epd.fill_rect(0, 106, 50, 50, 0xff)
+             epd.text(date, 0, 106, 0x00)
+             
+             epd.fill_rect(200, 106, 50, 50, 0xff)
+             epd.text(hourMin, 200, 106, 0x00)
+             
+             epd.display_Partial(epd.buffer)
+             
+         oldTime = hourMin
+         time.sleep(1)
